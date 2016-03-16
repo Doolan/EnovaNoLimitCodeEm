137b9b426ea88325c9174db5cc0a46827d14f639
Jared <hoffmaj2@hoffmaj2-ThinkPad-W530.(none)>
2016-03-15 20:12:34 -0400

    asdwad

---
+++ pkr.js
@@ -0,0 +1,14 @@
+var Stats = require('fast-stats').Stats;
+var express = require('express');
+var app = express();
+
+var args = process.argv.slice(2);
+
+app.get('/', function (req, res) {
+  res.write(args[0]);
+  res.end();
+});
+
+app.listen(parseInt(args[1]), function () {
+  console.log('Example app listening on port ' + args[1] + '!');
+});
