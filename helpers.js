2eeebcf67f838be359f595a9793bed9da22703c3
Steven Doolan <doolansr@rose-hulman.edu>
2016-03-15 21:13:35 -0400

    update logic

--- helpers.js
+++ helpers.js
@@ -35,31 +35,47 @@ var findN = function(hand, communityCards, idealCards){
 };
 
 
-var odds = function(n, numRemaining){
+var calcOdds = function(n, numRemaining){
+	if(n === 0){
+		return 1;
+	}
 	var deckSize = 52-7+numRemaining;
 
 };
 
 
-var royalFlushOdds = function(hand, communtiyCards, numRemaing){
+var royalFlushOdds = function(hand, communityCards, numRenaming){
 	var odds;
 	var acceptableHands = [['AH','KH','QH','JH','TH'],
 							['AD','KD','QD','JD','TD'],
 							['AC','KC','QC','JC','TC'],
 							['AS','KS','QS','JS','TS']];
 	for(var i = 0; i<acceptableHands.length; i++){
-		var n = findN(hand, communtiyCards, acceptableHands[i]);
-		//
+		var n = findN(hand, communityCards, acceptableHands[i]);
+		if(i !== -1){
+			odds += calcOdds(n, numRenaming)
+		}
 	}
+	//props should return something if guaranteed
+	return odds * ROYALMULT;
+};
+
+var straightFlushOdds = function (hand, communityCard, numRemaining) {
+	//any straight with 5 cards for same suit
 };
 
 
+var fullHouseOdds = function(hand, communityCard, numRemaining){
+	//3 of a kind and two of a kind
+};
 
 
+var flushOdds = function(hand, communityCard, numRemaining){
+	//five cards same suit
+};
 
-
-
-
-
+var straightOdds = function(hand, communityCard, numRemaining){
+	//five cards same suit
+};
 
 ///four three two pair one pair
